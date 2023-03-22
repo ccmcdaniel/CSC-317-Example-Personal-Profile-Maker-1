002cbbb4b1c574e8a2c0fe253b4a3dc079f14abb
Chad McDaniel <christopher.c.mcdaniel@usm.edu>
2023-03-22 14:22:16 -0500

    Fixed Birthday Calcuation to be accurate to Month and Day

--- Data Between Pages/Personal Page.xaml.cs
+++ Data Between Pages/Personal Page.xaml.cs
@@ -11,7 +11,7 @@ public partial class Personal_Page : ContentPage
 		int age = DateTime.Now.Year - App.birthday.Year;
 
 		if (DateTime.Now.Month - App.birthday.Month < 0 || 
-			DateTime.Now.Day - App.birthday.Day < 0)
+			(DateTime.Now.Month == App.birthday.Month && DateTime.Now.Day - App.birthday.Day < 0))
 			age -= 1;
 
 		lblAge.Text = $"{age} years old as of today.";
